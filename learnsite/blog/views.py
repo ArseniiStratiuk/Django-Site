41479708b8144121a454ad366e54f04e340d6084
ArseniiStratiuk <104721499+ArseniiStratiuk@users.noreply.github.com>
2022-12-18 00:38:36 +0200

    Update

--- learnsite/blog/views.py
+++ learnsite/blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+
 
 def blog_main(request, *args):
-    return render(request, 'blog_main.html', {})
+    posts = Post.objects.all()
+    data_dict = {
+        "posts": posts
+    }
+    return render(request, 'blog_main.html', data_dict)
