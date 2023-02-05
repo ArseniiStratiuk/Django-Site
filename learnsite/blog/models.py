5d8f9cbf8155f72a1eae5ad5bc4783c1c4edbd17
Arsenii Stratiuk <stratuik.777@gmail.com>
2023-02-05 12:22:12 +0200

    Update

--- learnsite/blog/models.py
+++ learnsite/blog/models.py
@@ -4,16 +4,31 @@ from PIL import Image
 from django.contrib.auth.models import User
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='blog/static/img/profiles', 
+                               default='blog/static/img/profiles/default.png', 
+                               verbose_name='Фото профілю')
+    about = models.TextField(max_length=500, verbose_name='Про себе', blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save()
+        img = Image.open(self.avatar.path)
+
+        if img.height > 120 or img.width > 120:
+            img.thumbnail((120, 120))
+            img.save(self.avatar.path)
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
-    post_slug = models.CharField(max_length=80, default="default_post")
-    img = models.ImageField(default="blog/static/img/default.png", 
-                            upload_to="blog/static/img", height_field=None,
+    post_slug = models.CharField(max_length=80, default='default_post')
+    img = models.ImageField(default='blog/static/img/default.png', 
+                            height_field=None, 
                             width_field=None, max_length=200, 
-                            verbose_name="Картинка для поста")
-    views_number = models.ManyToManyField(User, related_name="views_rating", blank=True)
+                            verbose_name='Картинка для поста')
+    views_number = models.ManyToManyField(User, related_name='views_rating', blank=True)
 
     def get_views_number(self):
         return self.views_number.count()
