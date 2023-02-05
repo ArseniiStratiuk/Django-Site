6dd223d7a53074c46779afcfec63e722a8f81fa2
Arsenii Stratiuk <stratuik.777@gmail.com>
2023-02-05 13:16:50 +0200

    Update

--- learnsite/blog/forms.py
+++ learnsite/blog/forms.py
@@ -3,9 +3,9 @@ from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
-    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class':'form-control-file'}))
-    about = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'rows': 4}))
-    
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    about = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4}))
+
     class Meta:
         model = Profile
         fields = ['avatar', 'about']
