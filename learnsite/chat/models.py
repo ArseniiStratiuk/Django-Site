7aad3f16fe1f99c7b4d98257ca735185c33b57e8
ArseniiStratiuk <104721499+ArseniiStratiuk@users.noreply.github.com>
2023-03-06 19:38:18 +0200

    Add sending messages feature

--- learnsite/chat/models.py
+++ learnsite/chat/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Message(models.Model):
-    sender = models.OneToOneField(User, on_delete=models.CASCADE, 
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, 
                                   related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, 
                                  related_name='received_messages')
--- learnsite/chat/urls.py
+++ learnsite/chat/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path(r'', views.load_messages_home),
+    path(r'', views.load_messages),
     path('search_user/', views.search_user, name="search_user"),
     # path('send_message/', views.send_message, name='send_message'),
     path('ajax/<int:pk>', views.load_msgAJAX, name="chatroom-ajax"),
