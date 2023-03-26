9c49a8c6201e172c15990328609454adaeb4a7b7
ArseniiStratiuk <104721499+ArseniiStratiuk@users.noreply.github.com>
2023-03-26 13:43:03 +0300

    Add email field to registration form

--- learnsite/blog/forms.py
+++ learnsite/blog/forms.py
@@ -1,6 +1,15 @@
 from django import forms
-from .models import Profile
-from .models import Comment
+from .models import Profile, Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(max_length=100, help_text="Обов'язкове поле")
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class ProfileForm(forms.ModelForm):
